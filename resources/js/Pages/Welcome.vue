2c5f81f539c70536f7f2fb7d0d412e6edd5bc9df
taw <tawan@admin>
2024-12-24 20:16:26 -0300

    enhance Welcome page layout; add icon and title for better user experience

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -151,9 +151,15 @@ const formatDate = (date) => {
   <Head title="Welcome" />
 
   <div class="flex min-h-screen bg-gray-50 text-gray-900">
-    <div v-if="$page.props.auth.user" class="p-6 border-r flex flex-col gap-4 w-72">
-      <Link :href="route('home')" class="text-lg font-medium hover:text-blue-600 transition duration-300">Início</Link>
-      <Link :href="route('dashboard')" class="text-lg font-medium hover:text-blue-600 transition duration-300">Perfil</Link>
+    <div v-if="$page.props.auth.user" class="p-6 border-r flex flex-col gap-4 items-center">
+      <Icon icon="openmoji:cat" class="text-6xl text-blue-600" />
+      <ElText class="text-2xl font-semibold text-gray-800 border-b-4">Dev Huddle</ElText>
+      <Link :href="route('home')" class="text-lg font-medium hover:text-blue-600 transition duration-300">
+        <ElText>Início</ElText>
+      </Link>
+      <Link :href="route('dashboard')" class="text-lg font-medium hover:text-blue-600 transition duration-300">
+        <ElText>Perfil</ElText>
+      </Link>
     </div>
 
     <div class="flex-1 p-24">
