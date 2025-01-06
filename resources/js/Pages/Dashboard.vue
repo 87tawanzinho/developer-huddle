ffeeba34e7508b8288f771ffbd9096094adffbc8
tawan <tawan@tecnovix.com>
2025-01-06 16:45:50 -0300

    xzfdsd

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -16,7 +16,8 @@ onMounted(() => {
 <template>
     <AppLayout title="Perfil">
         <template #header>
-            <div class="flex justify-between items-center pb-4 border-b-2 rounded-lg mt-16">
+           <div class="">
+             <div class="flex justify-between items-center pb-4 border-b-2 rounded-lg mt-16">
                 <div
                     class=" flex items-center gap-8    w-full"
                 >
@@ -25,6 +26,7 @@ onMounted(() => {
                         class="h-32 w-32 rounded-full"
                         alt=""
                     />
+                    {{ $page.props.auth.user.subscription }}
                     <ElText style="font-size: 32px">{{
                         $page.props.auth.user.name
                     }}</ElText>
@@ -32,6 +34,14 @@ onMounted(() => {
 
                 <Icon icon="mdi:settings" class="text-4xl text-gray-600 mt-4 transition-all hover:opacity-60 cursor-pointer" />
             </div>
+
+            <div class="mt-24 flex flex-col items-center justify-center">
+                <h2>Plano Atual</h2>
+                <div class="border rounded-2xl h-64 w-64 mt-4 text-center ">
+                    <span class="mt-2">{{ $page.props.auth.user.subscription.plan.name }}</span>
+                </div>
+            </div>
+           </div>
         </template>
     </AppLayout>
 </template>
